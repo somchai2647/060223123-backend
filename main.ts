bc56db1a67c1e02fe84431f3a57c20b020e5ee63
somchai2647 <somchai.b@cvc.ac.th>
2022-08-17 15:12:41 +0700

    setup server

---
+++ main.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
