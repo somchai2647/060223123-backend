de0b41eecde8d3e362623cc3c79b1874b84cf299
somchai2647 <somchai.b@cvc.ac.th>
2022-10-01 00:19:13 +0700

    + verifyToken

---
+++ middlewares/auth.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export function auth(req: Request, res: Response, next: NextFunction) {
+  const header = req.headers.authorization;
+  const token = header?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "No token, การอนุญาตถูกปฏิเสธ" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret");
+    //@ts-ignore
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token ไม่ถูกต้อง" });
+  }
+}
+
+export default auth;
--- routes/index.ts
+++ routes/index.ts
@@ -5,9 +5,13 @@ import category from './category.router';
 import auther from "./auther.router"
 import publisher from "./publisher.router"
 
+import verifyToken from '../middlewares/auth';
+
 const router = Router();
 
 router.use("/auth", auth);
+
+router.use(verifyToken);
 router.use("/product", product);
 router.use("/category", category);
 router.use("/auther", auther);
