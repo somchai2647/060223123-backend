270a3961ab45a73564e2f6a74b033f5ae258f962
somchai2647 <somchai.b@cvc.ac.th>
2022-10-01 14:57:48 +0700

    adjust

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -15,7 +15,7 @@ export function auth(req: Request, res: Response, next: NextFunction) {
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(400).json({ msg: "Token ไม่ถูกต้อง" });
+    res.status(400).json({ msg: "Token ไม่ถูกต้อง หรือ หมดอายุ" });
   }
 }
 
