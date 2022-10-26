f714872b42ad5b2333f6ed87a1397998c9396034
somchai2647 <somchai.b@cvc.ac.th>
2022-10-26 22:38:09 +0700

    adjust

--- controllers/cart.controller.ts
+++ controllers/cart.controller.ts
@@ -11,11 +11,11 @@ export async function createCart(req: Request, res: Response) {
           username: req.body.username,
         },
       },
-      Products:{
-        connect:{
-          id: req.body.productid
-        }
-      }
+      Products: {
+        connect: {
+          id: req.body.productid,
+        },
+      },
     };
     const cart = await prisma.cart.create({
       data: cartInput,
@@ -24,5 +24,46 @@ export async function createCart(req: Request, res: Response) {
   } catch (error) {
     console.log(error);
     res.status(400).json({ error });
+  }
+}
+
+export async function getCart(req: Request, res: Response) {
+  try {
+    //@ts-ignore
+    if (req.params.username !== req.user.username) {
+      throw Error("You are not authorized to view this cart");
+    }
+    const cart = await prisma.cart.findMany({
+      where: {
+        User: {
+          //@ts-ignore
+          username: req?.user?.username || "admin",
+        },
+      },
+      include: {
+        Products: {
+          include: {
+            image: {
+              orderBy: {
+                type: "asc",
+              },
+              select: {
+                url: true,
+                type: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (cart.length === 0) {
+      res.json([]);
+    } else {
+      res.json(cart);
+    }
+  } catch (error) {
+    console.log(error);
+    //@ts-ignore
+    res.status(400).json({ error: error.message });
   }
 }
--- routes/cart.router.ts
+++ routes/cart.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createCart } from "../controllers/cart.controller";
+import { createCart, getCart } from "../controllers/cart.controller";
+import verifyToken from "../middlewares/auth";
 const router = Router();
 
 router.post("/createCart", createCart);
-// router.get("/getCategory", getCategorys);
+router.get("/getCart/:username",  getCart);
 // router.get("/getCategory/:id", getCategory);
 // router.put("/updateCategory/:id", updateCategory);
 // router.delete("/destroyCategory/:id", destroyCategory);
